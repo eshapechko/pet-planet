f8b67304a38692cc9d83681df276fa56092987f7
Evgeniy Shapechko <shapen86@mail.ru>
2024-04-24 14:22:17 +0300

    add JS

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('.subscribe__input');
+const button = document.querySelector('.subscribe__button');
+
+input.addEventListener('focus', () => {
+  input.dataset.placeholder = input.placeholder;
+  input.placeholder = '';
+});
+
+input.addEventListener('blur', () => {
+  input.placeholder = input.dataset.placeholder;
+});
+
+input.addEventListener('input', ({ target }) => {
+  button.disabled = !target.value.trim();
+});
