bc54cd919d24bd582ec740523108c8235badda09
Evgeniy Shapechko <shapen86@mail.ru>
2024-05-30 09:55:44 +0300

    add JS to Cart

--- src/index.js
+++ src/index.js
@@ -24,6 +24,23 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const cartItemsList = document.querySelector('.modal__cart-items');
 const modalCloseButton = document.querySelector('.modal-overlay_close-button');
 const cartCount = cartButton.querySelector('.store__cart-cnt');
+const cartTotalPriceElement = document.querySelector('.modal__cart-price');
+const cartForm = document.querySelector('.modal__cart-form');
+
+const orderMessageElement = document.createElement('div');
+orderMessageElement.classList.add('order-message');
+
+const orderMessageText = document.createElement('p');
+orderMessageText.classList.add('order-message__text');
+
+const orderMessageCloseButton = document.createElement('button');
+orderMessageCloseButton.classList.add('order-message__close-button');
+orderMessageCloseButton.textContent = 'Закрыть';
+
+orderMessageElement.append(orderMessageText, orderMessageCloseButton);
+orderMessageCloseButton.addEventListener('click', () => {
+  orderMessageElement.remove();
+});
 
 const createProductCard = ({ id, photoUrl, name, price }) => {
   const productCard = document.createElement('li');
@@ -47,6 +64,7 @@ const createProductCard = ({ id, photoUrl, name, price }) => {
   return productCard;
 };
 
+// создаём список товаров
 const renderProducts = (products) => {
   productList.textContent = '';
 
@@ -71,7 +89,24 @@ const fetchProductByCategory = async (category) => {
 
     renderProducts(products);
   } catch (error) {
-    console.error(`Ошибка запроса товапов: ${error}`);
+    console.error(`Ошибка запроса товаров: ${error}`);
+  }
+};
+
+const fetchCartItems = async (ids) => {
+  try {
+    const response = await fetch(
+      `${API_URL}/api/products/list/${ids.join(',')}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`Ошибка запроса: ${response}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Ошибка запроса товаров  для корзины: ${error}`);
+    return [];
   }
 };
 
@@ -95,24 +130,70 @@ buttons.forEach((button) => {
   }
 });
 
+// считаем общую сумму корзины
+const calculateTotalPrice = (cartItems, products) =>
+  cartItems.reduce((acc, item) => {
+    const product = products.find((prod) => prod.id === item.id);
+    return acc + product.price * item.count;
+  }, 0);
+
 // создаём список товаров в корзине
-const renderCartItems = () => {
+const renderCartItems = async () => {
   cartItemsList.textContent = '';
   const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  const products = JSON.parse(
+    localStorage.getItem('cartProductDetails') || '[]',
+  );
 
-  cartItems.forEach((item) => {
+  products.forEach(({ id, photoUrl, name, price }) => {
+    const cartItem = cartItems.find((item) => item.id === id);
+
+    if (!cartItem) return;
+
     const listItem = document.createElement('li');
-    listItem.textContent = item;
+    listItem.classList.add('modal__cart-item');
+    listItem.innerHTML = `
+      <img class="modal__cart-item-image" src="${API_URL}/${photoUrl}" alt="${name}" />
+
+      <h3 class="modal__cart-item-title">${name}</h3>
+
+      <div class="modal__cart-item-count">
+        <button class="modal__btn modal__minus" data-id="${id}">-</button>
+        <span class="modal__count">${cartItem.count}</span>
+        <button class="modal__btn modal__plus" data-id="${id}">+</button>
+      </div>
+
+      <p class="modal__cart-item-price">${price * cartItem.count}&nbsp;₽</p>`;
+
     cartItemsList.append(listItem);
   });
+
+  const totalPrice = calculateTotalPrice(cartItems, products);
+  cartTotalPriceElement.innerHTML = `${totalPrice}&nbsp;₽`;
 };
 
 // открытие, закрытие корзины
-cartButton.addEventListener('click', () => {
+cartButton.addEventListener('click', async () => {
   modalOverlay.style.display = 'flex';
+
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  const ids = cartItems.map((item) => item.id);
+
+  if (!ids.length) {
+    cartItemsList.textContent = '';
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Корзина пуста';
+    cartItemsList.append(listItem);
+    return;
+  }
+
+  const products = await fetchCartItems(ids);
+  localStorage.setItem('cartProductDetails', JSON.stringify(products));
+
   renderCartItems();
 });
 
+// закрытие модального окна корзины
 modalOverlay.addEventListener('click', ({ target }) => {
   if (
     target === modalOverlay ||
@@ -130,10 +211,18 @@ const updateCartCount = () => {
 
 updateCartCount();
 
-const addToCart = (productName) => {
+// добавление в корзину
+const addToCart = (productId) => {
   const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
 
-  cartItems.push(productName);
+  const existingItem = cartItems.find((item) => item.id === productId);
+
+  if (existingItem) {
+    existingItem.count += 1;
+  } else {
+    cartItems.push({ id: productId, count: 1 });
+  }
+
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
   updateCartCount();
 };
@@ -141,10 +230,80 @@ const addToCart = (productName) => {
 // добавляем в корзину при клике по списку товаров
 productList.addEventListener('click', ({ target }) => {
   if (target.closest('.product__btn-add-cart')) {
-    console.log(target.dataset.id);
-    const productId = parseInt(target.dataset.id, 10);
-    console.log('productId: ', productId);
+    const productId = target.dataset.id;
 
     addToCart(productId);
   }
 });
+
+// обновление количества товаров при клике на плюс и минус
+const updateCartItem = (productId, change) => {
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  const itemIndex = cartItems.findIndex((item) => item.id === productId);
+
+  if (itemIndex !== -1) {
+    cartItems[itemIndex].count += change;
+
+    if (cartItems[itemIndex].count <= 0) {
+      cartItems.splice(itemIndex, 1);
+    }
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+    updateCartCount();
+    renderCartItems();
+  }
+};
+
+// изменение количества товаров при клике на плюс и минус
+cartItemsList.addEventListener('click', ({ target }) => {
+  if (target.classList.contains('modal__plus')) {
+    const productId = target.dataset.id;
+    updateCartItem(productId, 1);
+  }
+
+  if (target.classList.contains('modal__minus')) {
+    const productId = target.dataset.id;
+    updateCartItem(productId, -1);
+  }
+});
+
+// отправка заказа из формы корзины
+const submitOrder = async (e) => {
+  e.preventDefault();
+
+  const storeId = cartForm.store.value;
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+  const products = cartItems.map(({ id, count }) => ({
+    id,
+    quantity: count,
+  }));
+
+  try {
+    const response = await fetch(`${API_URL}/api/orders`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ storeId, products }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    localStorage.removeItem('cartItems');
+    localStorage.removeItem('cartProductDetails');
+
+    const { orderId } = await response.json();
+    orderMessageText.textContent = `Ваш заказ оформлен, номер заказа ${orderId}. Вы можете его забрать завтра после 12:00.`;
+    document.body.append(orderMessageElement);
+
+    modalOverlay.style.display = 'none';
+    updateCartCount();
+  } catch (error) {
+    console.error(`Ошибка оформления заказа: ${error}`);
+  }
+};
+
+cartForm.addEventListener('submit', submitOrder);
